ba05525ca443ebf77cdbfe9843150c50b27fca0f
paulcc <paulcc.two@gmail.com>
2009-07-23 15:47:07 +0100

    chg taxon link generation

--- app/helpers/sitemap_helper.rb
+++ app/helpers/sitemap_helper.rb
@@ -3,7 +3,7 @@ module SitemapHelper
   def sub_xml(xml, taxon, tree)
     tree.first.each do |p|
       xml.url {
-        xml.loc (@public_dir + p.permalink)                       # seo_url(taxon, p)
+        xml.loc (@public_dir + p.permalink)                       # was seo_url(taxon, p)
         xml.lastmod p.updated_at.xmlschema			  #change timestamp of last modified
         xml.changefreq 'weekly'
         xml.priority '0.8'
@@ -15,10 +15,10 @@ module SitemapHelper
   end
 
   def sub_html(xml, taxon, tree)
-    xml.p { xml.b { xml.a taxon.name.upcase, :href => taxon.permalink }}
+    xml.p { xml.b { xml.a taxon.name.upcase, :href => seo_url(taxon) }}
     xml.ul {
       tree.first.map do |p|
-        xml.li { xml.a p.name, :href => p.permalink }     # keep simple, seo_url(taxon, p) }
+        xml.li { xml.a p.name, :href => p.permalink }     # keep simple, was seo_url(taxon, p) }
       end
     }
     xml.ul {
